7066953ba7a7edf87f9ecd578918fa7af45f4b08
mousamax <mousamax101@gmail.com>
2024-10-04 21:49:20 +0200

    feat: integrate json validation

--- llm_api.py
+++ llm_api.py
@@ -1,15 +1,21 @@
 import os
 from typing import Any, Dict, List
 from together import Together
+from json_validation import JsonValidator
 
 
 class LLM_API:
     def __init__(self):
         self.client = Together(api_key=os.environ.get('TOGETHER_API_KEY'))
 
-    def get_response(self, model: str,  messages: List[Dict[str, Any]]):
+    def get_response(self, model: str,  messages: List[Dict[str, Any]]) -> str:
         response = self.client.chat.completions.create(
             model=model,
             messages=messages
         )
         return response.choices[0].message.content
+    
+    def get_json_response(self, model: str, messages: List[Dict[str, Any]], schema_file: str) -> dict | None:
+        response = self.get_response(model, messages)
+        json_validator = JsonValidator(schema_file)
+        return json_validator.get_json(response)
