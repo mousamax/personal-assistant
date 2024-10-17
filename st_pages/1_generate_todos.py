50737e425e407fb571bc74ba87389daac5bc4127
mousamax <mousamax101@gmail.com>
2024-10-17 15:56:47 +0200

    ignore gif

--- st_pages/1_generate_todos.py
+++ st_pages/1_generate_todos.py
@@ -15,13 +15,13 @@ st.markdown("### Let's get started! :rocket:")
 st.divider()
 
 # Collect user input
-user_query = st.text_input("Tell us what’s happening (e.g., “My child is sick and needs to be picked up from kindergarten”)", placeholder="Explain your situation here...")
+user_query = st.text_input("Tell us what’s happening (e.g., “The kindergarten called and my child is sick”)", placeholder="Explain your situation here...")
 
 # Generate todos when the user submits a query
 if st.button(":sparkles: Generate my smart task list"):
     if user_query:
         with st.spinner("familymind is generating your smart task list... :beer:"):
-            st.image("https://media.giphy.com/media/kyLYXonQYYfwYDIeZl/giphy.gif?cid=790b7611cicsergdo02m9pzp1t9v6sl3tjf8b72n0a5gotw8&ep=v1_gifs_trending&rid=giphy.gif&ct=g", use_column_width=True)
+            #st.image("https://media.giphy.com/media/kyLYXonQYYfwYDIeZl/giphy.gif?cid=790b7611cicsergdo02m9pzp1t9v6sl3tjf8b72n0a5gotw8&ep=v1_gifs_trending&rid=giphy.gif&ct=g", use_column_width=True)
             todos = tasks_agent.generate_todos(user_query)
             todos_view(todos)
     else:
